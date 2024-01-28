a582125651afa7228db3b6b8ad23e77a194dae8d
Shine0078 <128758030+Shine0078@users.noreply.github.com>
2024-01-28 12:46:56 -0500

    Update app.css

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,29 +1,52 @@
+"use strict";
+
 // IIFE - Immediately Invoked Function Expression
-// anonymous self-executing function
+// means -> anonymous self-executing function
 (function(){
+    // IIFE Scope
+    let h2;
+
+      
+
+    // Event Handlers
+    let MouseOver = function()
+    {
+        // MouseOver Scope
+        h2.style.color = "blue";
+        h2.style.fontWeight = "bold";
+        h2.textContent = "Mouse Over!!!";
+    }
+
+    function MouseOut()
+    {
+        h2.style.color = "black";
+        h2.style.fontWeight = "normal";
+        h2.textContent = "My Text Content";
+    }
 
     function Start()
     {
-        console.log("App Started...");
+        // Start Scope
 
-        let firstHeading = document.getElementById("firstHeading");
+        console.log("%c App Started", "color: blue; font-size: 20px;");
+        let mainContent = document.getElementById("mainContent");
 
-        firstHeading.textContent = "Goodbye cruel world!";
+        // create a new h2 tag with a class of Display-2 and "My Text Content" as the text node
+        h2 = document.createElement("h2");
+        h2.setAttribute("class","Display-2");
+        h2.textContent = "My Text Content";
+        mainContent.appendChild(h2);
 
-        //document.getElementById("firstHeading").textContent = "Goodbye cruel world!";
-    
-        firstHeading.style.fontWeight = "bold";
+        // event Listeners
+        h2.addEventListener("mouseover", MouseOver);
 
-        let main = document.getElementsByTagName("main")[0];
+        h2.addEventListener("mouseout", MouseOut);
 
-        // configuring my new element
-        let newH2 = document.createElement("h2");
-        newH2.setAttribute("class", "Display-5");
-        newH2.textContent = "secondary heading";
+        let brand = document.getElementsByClassName("navbar-brand")[0];
+        brand.textContent = "Client-Side Scripting";
 
-        main.appendChild(newH2);
-    
     }
+
 
     window.addEventListener("load", Start);
 })();
